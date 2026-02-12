6c9ff67401f98c16e20738329f12674b476a80a1
chandu2614 <chandu143.roc@gmail.com>
2026-02-12 10:36:13 +0530

    Add floorplan data for villa rooms

---
+++ floorplan-data.js
@@ -0,0 +1,44 @@
+// Floorplan data for Chandu's villa
+
+const floorplanData = {
+    groundFloor: {
+        rooms: {
+            garage: {dimensions: "2 cars"},
+            foyer: {},
+            drawingRoom: {dimensions: "17.9' x 12'", height: "double height (14')"},
+            kitchen: {dimensions: "10' - 3" x 12' - 7.5""},
+            kitchenDining: {dimensions: "14' - 9" x 10' - 4.5""},
+            lounge: {dimensions: "8' - 3" x 14' - 3""},
+            store: {dimensions: "7' - 9" x 5' - 0""},
+            servant: {dimensions: "7' - 9" x 6' - 0""},
+            homeTheater: {dimensions: "13' - 4" x 19' - 4""},
+            poolDeck: {dimensions: "8' - 3" x 12' - 4.5""},
+            swimmingPool: {dimensions: "9' - 0" x 18' - 0""},
+            openBar: {dimensions: "9' - 9" x 8' - 4.5""},
+            barCounter: {},
+            power: {},
+            toilet: {dimensions: "7' - 6" x 4' - 6""},
+            sitOut: {dimensions: "9' - 0" x 8' - 9""},
+            garden: {dimensions: "9' - 0" x 8' - 6""},
+            lift: {dimensions: "5' - 0" x 5' - 0""}         
+        },
+        wallHeight: "10 feet"
+    },
+    secondFloor: {
+        rooms: {
+            masterBedroom: {dimensions: "21' - 9" x 14' - 0""},
+            dress: {dimensions: "9' - 0" x 7' - 9""},
+            toilet1: {dimensions: "9' - 0" x 5' - 6""},
+            secondBedroom: {dimensions: "16' - 0" x 12' - 1.5""},
+            livingRoomOrOptionalBedroom: {dimensions: "21' - 9" x 12' - 0""},
+            puja: {},
+            openTerrace: {dimensions: "9' - 0" x 12' - 0""},
+            coveredTerrace: {dimensions: "9' - 0" x 12' - 10.5""},
+            walkInCloset: {dimensions: "7' - 9" x 5' - 0""},
+            toilet2: {dimensions: "5' - 4" x 7' - 6""}
+        },
+        wallHeight: "10 feet"
+    }
+};
+
+export default floorplanData;
